3d790829a3f8abb57b95858d72d36b182248bbba
Veins (password is veins) <veins@instant-veins.car2x.org>
2025-05-16 09:53:22 +0000

    dir ok

---
+++ out/clang-debug/src/veins_inet/VeinsInetManagerBase.o.d
@@ -0,0 +1,211 @@
+../out/clang-debug/src/veins_inet/VeinsInetManagerBase.o: \
+  veins_inet/VeinsInetManagerBase.cc \
+  /home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetManagerBase.h \
+  /home/veins/src/veins/subprojects/veins_inet/src/veins_inet/veins_inet.h \
+  ../../veins/src/veins/veins.h \
+  /home/veins/src/omnetpp-5.7/include/omnetpp.h \
+  ../../inet/src/inet/common/INETDefs.h \
+  ../../inet/src/inet/common/precompiled_debug.h \
+  ../../inet/src/inet/common/precompiled.h \
+  ../../inet/src/inet/opp_defines.h ../../inet/src/inet/features.h \
+  ../../inet/src/inet/common/Compat.h \
+  ../../inet/src/inet/common/InitStages.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+  ../../veins/src/veins/base/utils/Coord.h \
+  ../../veins/src/veins/base/utils/FWMath.h \
+  ../../veins/src/veins/base/modules/BaseWorldUtility.h \
+  ../../veins/src/veins/base/connectionManager/BaseConnectionManager.h \
+  ../../veins/src/veins/base/utils/AntennaPosition.h \
+  ../../veins/src/veins/base/connectionManager/NicEntry.h \
+  ../../veins/src/veins/base/utils/Heading.h \
+  ../../veins/src/veins/modules/utility/HasLogProxy.h \
+  ../../veins/src/veins/base/utils/FindModule.h \
+  ../../veins/src/veins/modules/obstacle/ObstacleControl.h \
+  ../../veins/src/veins/modules/obstacle/Obstacle.h \
+  ../../veins/src/veins/modules/world/annotations/AnnotationManager.h \
+  ../../veins/src/veins/modules/utility/BBoxLookup.h \
+  ../../veins/src/veins/modules/obstacle/VehicleObstacleControl.h \
+  ../../veins/src/veins/base/utils/Move.h \
+  ../../veins/src/veins/modules/obstacle/MobileHostObstacle.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIBuffer.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIConstants.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIColor.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIConnection.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICoord.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  ../../veins/src/veins/modules/mobility/traci/VehicleSignal.h \
+  ../../veins/src/veins/base/utils/EnumBitset.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  ../../veins/src/veins/modules/utility/SignalManager.h \
+  /home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetMobility.h \
+  ../../inet/src/inet/mobility/base/MobilityBase.h \
+  ../../inet/src/inet/common/ModuleAccess.h \
+  ../../inet/src/inet/networklayer/contract/IInterfaceTable.h \
+  ../../inet/src/inet/networklayer/common/InterfaceEntry.h \
+  ../../inet/src/inet/common/packet/tag/TagSet.h \
+  ../../inet/src/inet/common/Simsignals.h \
+  ../../inet/src/inet/common/Simsignals_m.h \
+  ../../inet/src/inet/common/INETDefs_m.h \
+  ../../inet/src/inet/linklayer/common/MacAddress.h \
+  ../../inet/src/inet/networklayer/common/InterfaceToken.h \
+  ../../inet/src/inet/networklayer/common/L3Address.h \
+  ../../inet/src/inet/networklayer/common/ModuleIdAddress.h \
+  ../../inet/src/inet/networklayer/common/ModulePathAddress.h \
+  ../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h \
+  ../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h \
+  ../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h \
+  ../../inet/src/inet/common/StringFormat.h \
+  ../../inet/src/inet/common/geometry/common/CanvasProjection.h \
+  ../../inet/src/inet/common/geometry/common/RotationMatrix.h \
+  ../../inet/src/inet/common/geometry/common/Coord.h \
+  ../../inet/src/inet/common/INETMath.h \
+  ../../inet/src/inet/common/geometry/common/EulerAngles.h \
+  ../../inet/src/inet/common/Units.h \
+  ../../inet/src/inet/common/geometry/common/Quaternion.h \
+  ../../inet/src/inet/mobility/contract/IMobility.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICommandInterface.h \
+  ../../veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h \
+  ../../inet/src/inet/common/scenario/ScenarioManager.h \
+  ../../inet/src/inet/common/lifecycle/LifecycleController.h \
+  ../../inet/src/inet/common/lifecycle/ILifecycle.h \
+  ../../inet/src/inet/common/scenario/IScriptable.h
+
+/home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetManagerBase.h:
+
+/home/veins/src/veins/subprojects/veins_inet/src/veins_inet/veins_inet.h:
+
+../../veins/src/veins/veins.h:
+
+/home/veins/src/omnetpp-5.7/include/omnetpp.h:
+
+../../inet/src/inet/common/INETDefs.h:
+
+../../inet/src/inet/common/precompiled_debug.h:
+
+../../inet/src/inet/common/precompiled.h:
+
+../../inet/src/inet/opp_defines.h:
+
+../../inet/src/inet/features.h:
+
+../../inet/src/inet/common/Compat.h:
+
+../../inet/src/inet/common/InitStages.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+../../veins/src/veins/base/utils/Coord.h:
+
+../../veins/src/veins/base/utils/FWMath.h:
+
+../../veins/src/veins/base/modules/BaseWorldUtility.h:
+
+../../veins/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+../../veins/src/veins/base/utils/AntennaPosition.h:
+
+../../veins/src/veins/base/connectionManager/NicEntry.h:
+
+../../veins/src/veins/base/utils/Heading.h:
+
+../../veins/src/veins/modules/utility/HasLogProxy.h:
+
+../../veins/src/veins/base/utils/FindModule.h:
+
+../../veins/src/veins/modules/obstacle/ObstacleControl.h:
+
+../../veins/src/veins/modules/obstacle/Obstacle.h:
+
+../../veins/src/veins/modules/world/annotations/AnnotationManager.h:
+
+../../veins/src/veins/modules/utility/BBoxLookup.h:
+
+../../veins/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+../../veins/src/veins/base/utils/Move.h:
+
+../../veins/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIColor.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICoord.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+../../veins/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+../../veins/src/veins/base/utils/EnumBitset.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+../../veins/src/veins/modules/utility/SignalManager.h:
+
+/home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetMobility.h:
+
+../../inet/src/inet/mobility/base/MobilityBase.h:
+
+../../inet/src/inet/common/ModuleAccess.h:
+
+../../inet/src/inet/networklayer/contract/IInterfaceTable.h:
+
+../../inet/src/inet/networklayer/common/InterfaceEntry.h:
+
+../../inet/src/inet/common/packet/tag/TagSet.h:
+
+../../inet/src/inet/common/Simsignals.h:
+
+../../inet/src/inet/common/Simsignals_m.h:
+
+../../inet/src/inet/common/INETDefs_m.h:
+
+../../inet/src/inet/linklayer/common/MacAddress.h:
+
+../../inet/src/inet/networklayer/common/InterfaceToken.h:
+
+../../inet/src/inet/networklayer/common/L3Address.h:
+
+../../inet/src/inet/networklayer/common/ModuleIdAddress.h:
+
+../../inet/src/inet/networklayer/common/ModulePathAddress.h:
+
+../../inet/src/inet/networklayer/contract/clns/ClnsAddress.h:
+
+../../inet/src/inet/networklayer/contract/ipv4/Ipv4Address.h:
+
+../../inet/src/inet/networklayer/contract/ipv6/Ipv6Address.h:
+
+../../inet/src/inet/common/StringFormat.h:
+
+../../inet/src/inet/common/geometry/common/CanvasProjection.h:
+
+../../inet/src/inet/common/geometry/common/RotationMatrix.h:
+
+../../inet/src/inet/common/geometry/common/Coord.h:
+
+../../inet/src/inet/common/INETMath.h:
+
+../../inet/src/inet/common/geometry/common/EulerAngles.h:
+
+../../inet/src/inet/common/Units.h:
+
+../../inet/src/inet/common/geometry/common/Quaternion.h:
+
+../../inet/src/inet/mobility/contract/IMobility.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICommandInterface.h:
+
+../../veins/src/veins/modules/world/traci/trafficLight/TraCITrafficLightProgram.h:
+
+../../inet/src/inet/common/scenario/ScenarioManager.h:
+
+../../inet/src/inet/common/lifecycle/LifecycleController.h:
+
+../../inet/src/inet/common/lifecycle/ILifecycle.h:
+
+../../inet/src/inet/common/scenario/IScriptable.h:
---
+++ out/clang-release/src/veins_inet/VeinsInetManagerForker.o.d
@@ -0,0 +1,124 @@
+../out/clang-release/src/veins_inet/VeinsInetManagerForker.o: \
+  veins_inet/VeinsInetManagerForker.cc \
+  /home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetManagerForker.h \
+  /home/veins/src/veins/subprojects/veins_inet/src/veins_inet/veins_inet.h \
+  ../../veins/src/veins/veins.h \
+  /home/veins/src/omnetpp-5.7/include/omnetpp.h \
+  ../../inet/src/inet/common/INETDefs.h \
+  ../../inet/src/inet/common/precompiled_release.h \
+  ../../inet/src/inet/common/precompiled.h \
+  ../../inet/src/inet/opp_defines.h ../../inet/src/inet/features.h \
+  ../../inet/src/inet/common/Compat.h \
+  ../../inet/src/inet/common/InitStages.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIScenarioManagerForker.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h \
+  ../../veins/src/veins/base/utils/Coord.h \
+  ../../veins/src/veins/base/utils/FWMath.h \
+  ../../veins/src/veins/base/modules/BaseWorldUtility.h \
+  ../../veins/src/veins/base/connectionManager/BaseConnectionManager.h \
+  ../../veins/src/veins/base/utils/AntennaPosition.h \
+  ../../veins/src/veins/base/connectionManager/NicEntry.h \
+  ../../veins/src/veins/base/utils/Heading.h \
+  ../../veins/src/veins/modules/utility/HasLogProxy.h \
+  ../../veins/src/veins/base/utils/FindModule.h \
+  ../../veins/src/veins/modules/obstacle/ObstacleControl.h \
+  ../../veins/src/veins/modules/obstacle/Obstacle.h \
+  ../../veins/src/veins/modules/world/annotations/AnnotationManager.h \
+  ../../veins/src/veins/modules/utility/BBoxLookup.h \
+  ../../veins/src/veins/modules/obstacle/VehicleObstacleControl.h \
+  ../../veins/src/veins/base/utils/Move.h \
+  ../../veins/src/veins/modules/obstacle/MobileHostObstacle.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIBuffer.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIConstants.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIColor.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIConnection.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICoord.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h \
+  ../../veins/src/veins/modules/mobility/traci/VehicleSignal.h \
+  ../../veins/src/veins/base/utils/EnumBitset.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h \
+  ../../veins/src/veins/modules/mobility/traci/TraCILauncher.h \
+  /home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetManagerBase.h \
+  ../../veins/src/veins/modules/utility/SignalManager.h
+
+/home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetManagerForker.h:
+
+/home/veins/src/veins/subprojects/veins_inet/src/veins_inet/veins_inet.h:
+
+../../veins/src/veins/veins.h:
+
+/home/veins/src/omnetpp-5.7/include/omnetpp.h:
+
+../../inet/src/inet/common/INETDefs.h:
+
+../../inet/src/inet/common/precompiled_release.h:
+
+../../inet/src/inet/common/precompiled.h:
+
+../../inet/src/inet/opp_defines.h:
+
+../../inet/src/inet/features.h:
+
+../../inet/src/inet/common/Compat.h:
+
+../../inet/src/inet/common/InitStages.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIScenarioManagerForker.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIScenarioManager.h:
+
+../../veins/src/veins/base/utils/Coord.h:
+
+../../veins/src/veins/base/utils/FWMath.h:
+
+../../veins/src/veins/base/modules/BaseWorldUtility.h:
+
+../../veins/src/veins/base/connectionManager/BaseConnectionManager.h:
+
+../../veins/src/veins/base/utils/AntennaPosition.h:
+
+../../veins/src/veins/base/connectionManager/NicEntry.h:
+
+../../veins/src/veins/base/utils/Heading.h:
+
+../../veins/src/veins/modules/utility/HasLogProxy.h:
+
+../../veins/src/veins/base/utils/FindModule.h:
+
+../../veins/src/veins/modules/obstacle/ObstacleControl.h:
+
+../../veins/src/veins/modules/obstacle/Obstacle.h:
+
+../../veins/src/veins/modules/world/annotations/AnnotationManager.h:
+
+../../veins/src/veins/modules/utility/BBoxLookup.h:
+
+../../veins/src/veins/modules/obstacle/VehicleObstacleControl.h:
+
+../../veins/src/veins/base/utils/Move.h:
+
+../../veins/src/veins/modules/obstacle/MobileHostObstacle.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIBuffer.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIConstants.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIColor.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIConnection.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICoord.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCICoordinateTransformation.h:
+
+../../veins/src/veins/modules/mobility/traci/VehicleSignal.h:
+
+../../veins/src/veins/base/utils/EnumBitset.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCIRegionOfInterest.h:
+
+../../veins/src/veins/modules/mobility/traci/TraCILauncher.h:
+
+/home/veins/src/veins/subprojects/veins_inet/src/veins_inet/VeinsInetManagerBase.h:
+
+../../veins/src/veins/modules/utility/SignalManager.h:
